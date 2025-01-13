530beaab13360ae2448631fd8b0d92e5be4e4bd6
Rittika Dev <rittika.dev@gmail.com>
2025-01-13 18:28:26 +0600

    type safety added later

--- src/redux/features/auth/authSlice.ts
+++ src/redux/features/auth/authSlice.ts
@@ -1,30 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { RootState } from '../../store';
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
 
 type TAuthState = {
-  user: null | object;
-  token: null | string;
+	user: null | object;
+	token: null | string;
 };
 
 const initialState: TAuthState = {
-  user: null,
-  token: null,
+	user: null,
+	token: null,
 };
 
 const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {
-    setUser: (state, action) => {
-      const { user, token } = action.payload;
-      state.user = user;
-      state.token = token;
-    },
-    logout: (state) => {
-      state.user = null;
-      state.token = null;
-    },
-  },
+	name: "auth",
+	initialState,
+	reducers: {
+		setUser: (state, action) => {
+			const { user, token } = action.payload;
+			state.user = user;
+			state.token = token;
+		},
+		logout: (state) => {
+			state.user = null;
+			state.token = null;
+		},
+	},
 });
 
 export const { setUser, logout } = authSlice.actions;
