906aae7253c65cf7a30761f28d8733015151017a
RittikaDev <rittika.dev@gmail.com>
2025-01-15 22:43:22 +0600

    fixed imports

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,5 +1,7 @@
 import { Layout, Menu } from "antd";
+
 import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+
 import { adminPaths } from "../../routes/admin.routes";
 import { facultyPaths } from "../../routes/faculty.routes";
 import { studentPaths } from "../../routes/student.routes";
@@ -7,51 +9,51 @@ import { studentPaths } from "../../routes/student.routes";
 const { Sider } = Layout;
 
 const userRole = {
-	ADMIN: "admin",
-	FACULTY: "faculty",
-	STUDENT: "student",
+  ADMIN: "admin",
+  FACULTY: "faculty",
+  STUDENT: "student",
 };
 
 const Sidebar = () => {
-	const role = "student";
-	let sidebarItems;
+  const role = "student";
+  let sidebarItems;
 
-	switch (role) {
-		case userRole.ADMIN:
-			sidebarItems = sidebarItemsGenerator(adminPaths, userRole.ADMIN);
-			break;
-		case userRole.FACULTY:
-			sidebarItems = sidebarItemsGenerator(facultyPaths, userRole.FACULTY);
-			break;
-		case userRole.STUDENT:
-			sidebarItems = sidebarItemsGenerator(studentPaths, userRole.STUDENT);
-			break;
+  switch (role) {
+    case userRole.ADMIN:
+      sidebarItems = sidebarItemsGenerator(adminPaths, userRole.ADMIN);
+      break;
+    case userRole.FACULTY:
+      sidebarItems = sidebarItemsGenerator(facultyPaths, userRole.FACULTY);
+      break;
+    case userRole.STUDENT:
+      sidebarItems = sidebarItemsGenerator(studentPaths, userRole.STUDENT);
+      break;
 
-		default:
-			break;
-	}
+    default:
+      break;
+  }
 
-	return (
-		<Sider breakpoint="lg" collapsedWidth="0">
-			<div
-				style={{
-					color: "white",
-					height: "4rem",
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-				}}
-			>
-				<h1>PH Uni</h1>
-			</div>
-			<Menu
-				theme="dark"
-				mode="inline"
-				defaultSelectedKeys={["4"]}
-				items={sidebarItems}
-			/>
-		</Sider>
-	);
+  return (
+    <Sider breakpoint="lg" collapsedWidth="0">
+      <div
+        style={{
+          color: "white",
+          height: "4rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>PH Uni</h1>
+      </div>
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={["4"]}
+        items={sidebarItems}
+      />
+    </Sider>
+  );
 };
 
 export default Sidebar;
